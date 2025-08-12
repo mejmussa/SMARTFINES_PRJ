849431ff9eb50b48f851b07b0416392f135d73cc
mejmussa <mejmussa@gmail.com>
2025-08-12 17:01:51 +0300

    AUG 12 2025

--- monitoring/views.py
+++ monitoring/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def index(request):
-    if request.user.is_authenticated:
-        # Redirect to business selection page
-        return redirect('index')  # You must define this name in urls.py
     return render(request, 'monitoring/index.html', {'user': None})
