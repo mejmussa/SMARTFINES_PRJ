db9f08ce3d0bfa4899f6986a15ef499bfc501da5
mejmussa <mejmussa@gmail.com>
2025-09-24 16:33:02 +0300

    24 SEP 2025

--- core/views.py
+++ core/views.py
@@ -13,10 +13,10 @@ from django.core.files.base import ContentFile
 
 
 # ✅ Update this with your real plain text client secret
-# DEV SECRETS
-CLIENT_ID = 'RjT1NqgwjRzqB2OEe91yMBpw7vnva18tbsd5468z'
-CLIENT_SECRET = 'MNslO7EADcJdi11phwS4WiuKL5OGHTE45eaCNTWusGyUrEonSYJjcw6gsVBoj5SEgTsQg1O79BCXyao3zBbQW3czuHYi22UUFM9cBnX7BuwnterrwcjKSvSF7mZg0Xp5'
-REDIRECT_URI = 'http://127.0.0.1:8000/oauth/callback/'
+# PRODUCTION SECRETS
+CLIENT_ID = 'z9WF2rpGCegt8JnosLtVpzf2T9HRmuzPTrgQy2qr'
+CLIENT_SECRET='VerbwziHwAx0oICxZrdz7tQu4JZMDiYOTWy7t8jBiUCaqkNgXXESlq8LTyppZpKLobhC4X3sevyXzbaAEARf0SeiruxU8y3zRD0GLzGJjdfQUV1Srqqw9Z8S7Ne0SaWr'
+REDIRECT_URI = 'https://smartfines.net/oauth/callback/'
 AUTH_SERVER_BASE_URL = 'https://www.tecmocsy.com'  # ❌ no trailing slash
 
 
